20fd5ac198945607f0323bd5418e31043b8b2bfd
Laura Rocha <lauradevsrocha@gmail.com>
2025-10-31 19:46:57 -0300

    Delete projetoIntegrador directory

--- projetoIntegrador/lib/constants.js
+++
@@ -1,8 +0,0 @@
-export const projetosIniciais = [
-    { id: 1, nome: "Gestão do Projeto Integrador", data: "2025-11-30", membros: "Evelyn, Laura, Pedro Alves, Pedro Costa", convidados: "Não", observacao: "Projeto para organização de outros projetos" },
-    { id: 2, nome: "Biblioteca do Senac", data: "2025-11-03", membros: "Carlos, Ana, João", convidados: "Não", observacao: "Demonstração funcional" },
-    { id: 3, nome: "Encontre uma república", data: "2025-11-25", membros: "Jairo, Lucas, Bia", convidados: "Sim", observacao: "Trabalho em equipe" },
-    { id: 4, nome: "Adote um animal", data: "2025-12-10", membros: "Fernanda, Julia, Leticia", convidados: "Sim", observacao: "Presença de ONGs" },
-    { id: 5, nome: "Aluguel de vestidos para casamento", data: "2025-12-17", membros: "Rafael, Sofia, Caio", convidados: "Não", observacao: "Apresentação com protótipo avançado" },
-    { id: 6, nome: "App de corridas urbanas", data: "2025-11-22", membros: "Bolsonaro, Lula, Trump", convidados: "Sim", observacao: "App quase finalizado" },
-  ];
--- projetoIntegrador/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: 'class', // ativa modo escuro controlado por classe
-    content: ['./src/**/*.{js,jsx,ts,tsx,html}'], // ajuste conforme sua estrutura
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
