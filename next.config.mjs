6efb7de9e8ebb0469ae75f49b8f4483a4a70ad5c
Laura Rocha <lauradevsrocha@gmail.com>
2025-12-03 21:43:04 -0300

    atualização do supabase com imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,25 @@
 import withPWA from "next-pwa";
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "blobs.vercel.com",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ivflshwbkgezpnejhhje.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 const isProd = process.env.NODE_ENV === "development" ? false : true;
 // Configuração do PWA
 const withPWAConfig = withPWA({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: !isProd,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: !isProd,
 })(nextConfig);
- 
+
 export default withPWAConfig;
